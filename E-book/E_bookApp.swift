ee0c0cca5e856e6b7b9f3a5fb0a97f5ef70427de
Abo <abubker630a@gmail.com>
2022-12-14 10:02:43 +0500

    Initial Commit

---
+++ E-book/E_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_bookApp.swift
+//  E-book
+//
+//  Created by Аbubakr Futtaini on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct E_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
