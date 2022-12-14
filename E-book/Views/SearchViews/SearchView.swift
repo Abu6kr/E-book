7c39d0077f93bfec4e14d1b4ed3de0be9d1f5c3e
abo <abubker630a@gmail.com>
2022-12-14 13:54:45 +0500

    add search view

---
+++ E-book/Views/SearchViews/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  E-book
+//
+//  Created by Аbubakr Futtaini on 14.12.2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
