f8f330cf785c784d795e10659f8d4097562cf50a
abo <abubker630a@gmail.com>
2022-12-15 12:31:58 +0500

    loading view

---
+++ E-book/OnboardingViews/Loading.swift
@@ -0,0 +1,20 @@
+//
+//  Loading.swift
+//  E-book
+//
+//  Created by Аbubakr Futtaini on 15.12.2022.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Loading_Previews: PreviewProvider {
+    static var previews: some View {
+        Loading()
+    }
+}
