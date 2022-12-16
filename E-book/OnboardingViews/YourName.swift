e4ff032709d5da74cdf0ba43224bfe00c9b92be9
abo <abubker630a@gmail.com>
2022-12-16 12:00:10 +0500

    Make models

---
+++ E-book/OnboardingViews/YourName.swift
@@ -0,0 +1,20 @@
+//
+//  YourName.swift
+//  E-book
+//
+//  Created by Аbubakr Futtaini on 16.12.2022.
+//
+
+import SwiftUI
+
+struct YourName: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YourName_Previews: PreviewProvider {
+    static var previews: some View {
+        YourName()
+    }
+}
